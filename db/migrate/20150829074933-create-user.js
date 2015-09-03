ae3b5254e919807cd68dfbfb0643dd3690ad3563
meddle <meddlesmerch@gmail.com>
2015-09-04 00:37:19 +0300

    The migrations

---
+++ db/migrate/20150829074933-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, {
+      underscored: true,
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('users');
+  }
+};
